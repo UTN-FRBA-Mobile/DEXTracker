366199a51b6142c6279c449f75bac59009bc2b03
Alejandro <aleperaltabazas@hotmail.com>
2021-05-16 17:26:05 -0300

    agrego vista basica del PokedexActivity

---
+++ app/src/main/java/com/github/utn/frba/mobile/dextracker/PokedexActivity.kt
@@ -0,0 +1,13 @@
+package com.github.utn.frba.mobile.dextracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PokedexActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokedex)
+
+    }
+}
