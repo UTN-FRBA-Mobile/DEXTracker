10fbaef42d527228d52cbffcf605a74fbfe5b9f6
Alejandro <aleperaltabazas@hotmail.com>
2021-05-17 19:34:42 -0300

    agrego un approach super naive para el marcado de los pokemones como atrapados

--- app/src/main/java/com/github/utn/frba/mobile/dextracker/PokedexActivity.kt
+++ app/src/main/java/com/github/utn/frba/mobile/dextracker/PokedexActivity.kt
@@ -2,6 +2,7 @@ package com.github.utn.frba.mobile.dextracker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.utn.frba.mobile.dextracker.ui.pokedex.PokemonFragment
 
 class PokedexActivity : AppCompatActivity() {
 
@@ -9,5 +10,32 @@ class PokedexActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokedex)
 
+        supportFragmentManager.beginTransaction().apply {
+            add(
+                R.id.pokemon_table,
+                PokemonFragment.newInstance(
+                    pokemonName = "bulbasaur",
+                    pokemonPictureResourceId = R.drawable.placeholder,
+                )
+            )
+            add(
+                R.id.pokemon_table,
+                PokemonFragment.newInstance(
+                    pokemonName = "ivysaur",
+                    pokemonPictureResourceId = R.drawable.placeholder,
+                )
+            )
+            add(
+                R.id.pokemon_table,
+                PokemonFragment.newInstance(
+                    pokemonName = "venusaur",
+                    pokemonPictureResourceId = R.drawable.placeholder,
+                )
+            )
+
+            commit()
+        }
     }
+
+
 }
--- app/src/main/java/com/github/utn/frba/mobile/dextracker/ui/pokedex/PokemonFragment.kt
+++ app/src/main/java/com/github/utn/frba/mobile/dextracker/ui/pokedex/PokemonFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.github.utn.frba.mobile.dextracker.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val POKEMON_NAME = "pokemon_name"
+private const val POKEMON_PICTURE = "pokemon_picture"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,24 +19,39 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PokemonFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var pokemonName: String? = null
+    private var pokemonPictureResourceId: Int? = null
+
+    private var caught: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            pokemonName = it.getString(POKEMON_NAME)
+            pokemonPictureResourceId = it.getInt(POKEMON_PICTURE)
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokemon, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_pokemon, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<LinearLayout>(R.id.bg).setOnClickListener {
+            caught = !caught
+            val color = if (caught) R.color.green else R.color.white
+            it.setBackgroundResource(color)
+        }
+
+        view.findViewById<TextView>(R.id.pokemon_name).apply {
+            text = pokemonName
+        }
+
+        view.findViewById<ImageView>(R.id.pokemon_picture).apply {
+            this.setImageResource(pokemonPictureResourceId!!)
+        }
     }
 
     companion object {
@@ -43,17 +59,16 @@ class PokemonFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         * @param pokemonName Parameter 1.
+         * @param pokemonPictureResourceId Parameter 2.
          * @return A new instance of fragment PokemonFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(pokemonName: String, pokemonPictureResourceId: Int) =
             PokemonFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(POKEMON_NAME, pokemonName)
+                    putInt(POKEMON_PICTURE, pokemonPictureResourceId)
                 }
             }
     }
